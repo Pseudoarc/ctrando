41fd5b9911db478ac063eb50ad79166c8b23e060
pseudoarc <indecomposable@gmail.com>
2025-10-23 14:05:59 -0400

    Do not require Robo active (just recruited) for entering Factory.

--- src/ctrando/base/openworld/factoryentrance.py
+++ src/ctrando/base/openworld/factoryentrance.py
@@ -21,34 +21,46 @@ class EventMod(locationevent.LocEventMod):
           activated without Robo (no partyfollow)
         """
 
+        _, copy_cmd = script.find_command(
+            [0xE5], script.get_function_start(4, FID.ARBITRARY_2)
+        )
+
         pos = script.get_function_start(8, FID.ACTIVATE)
         pos = script.find_exact_command(EC.if_pc_active(ctenums.CharID.ROBO), pos)
 
-        # It's annoying to add an "else" after the existing check.  So we add a
-        # slightly redundant if-else prior to the existing if.
-        script.insert_commands(
+        block = (
             EF().add_if_else(
-                EC.if_pc_active(ctenums.CharID.ROBO),
-                EF(),
+                EC.if_pc_recruited(ctenums.CharID.ROBO),
+                EF()
+                .add(EC.play_sound(0x70))
+                .add(copy_cmd)
+                .add(EC.pause(2))
+                .add(EC.play_sound(0xA0)),
                 EF().add(EC.auto_text_box(
                     script.add_py_string("ERROR: Only R-Series can access.{null}")
                 ))
-            ).get_bytearray(), pos
+            )
         )
+
+        script.insert_commands(
+            block.get_bytearray(), pos
+        )
+        pos += len(block)
+        script.delete_jump_block(pos)
 
         # Slightly change the party positioning for the computer activation.
         # It just looks weird for Robo to run up to the computer, run away, and then
         # run back.
-        pos, _ = script.find_command([0xD9], pos)
-        script.data[pos+1: pos+7] = [9, 0x15, 7, 0x15, 0xB, 0x15]
-
-        move_pos = script.find_exact_command(
-            EC.pause(3), script.get_function_start(4, FID.ARBITRARY_2)
-        )
-        script.data[move_pos+1] = 0x18
-        script.insert_commands(
-            EC.set_own_facing('up').to_bytearray(), move_pos
-        )
+        # pos, _ = script.find_command([0xD9], pos)
+        # script.data[pos+1: pos+7] = [9, 0x15, 7, 0x15, 0xB, 0x15]
+        #
+        # move_pos = script.find_exact_command(
+        #     EC.pause(3), script.get_function_start(4, FID.ARBITRARY_2)
+        # )
+        # script.data[move_pos+1] = 0x18
+        # script.insert_commands(
+        #     EC.set_own_facing('up').to_bytearray(), move_pos
+        # )
 
         pos = script.find_exact_command(
             EC.if_mem_op_value(0x7F0214, OP.EQUALS, 0), pos
