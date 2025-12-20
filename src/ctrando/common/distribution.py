b419182414a7d6528eebbf79ed39898f90e6ca4c
pseudoarc <indecomposable@gmail.com>
2025-12-20 09:28:28 -0500

    Add DistributionGenerator to automate making distributions from strings.

--- src/ctrando/common/distribution.py
+++ src/ctrando/common/distribution.py
@@ -1,21 +1,25 @@
-'''
+"""
 Implement Distribution objects.
 
 A Distribtuion is just a collection of (weight, value_list) pairs.
 When generating a random item from the distribution, pick a pair based on
 the weights, then return a random element of the pair's value_list.
-'''
+"""
 
 from __future__ import annotations
+
+import copy
+import itertools
+import random
 # import random
-from collections.abc import Iterable
+from collections.abc import Iterable, Sequence
 import typing
 
 from ctrando.common.random import RNGType
 
 
 T = typing.TypeVar('T')
-ObjType = typing.Union[T, typing.Sequence[T]]
+ObjType = typing.Union[T, Sequence[T]]
 WeightType = typing.Union[int, float]
 
 
@@ -148,3 +152,80 @@ class Distribution(typing.Generic[T]):
                 new_pairs.append((weight, trimmed_vals))
 
         return Distribution[T](*new_pairs)
+
+
+class DistributionGenerator(typing.Generic[T]):
+    def __init__(self, symbol_dict: dict[str, Sequence[T]]):
+        """
+        Make a new Distribution Generator.  Note that symbol_dict should be
+        sequences (e.g.  {"wood_sword": [ItemID.WoodSword]})
+        """
+        self.symbol_dict = copy.deepcopy(symbol_dict)
+        self.singleton_dict = {
+            key: val[0] for key,val in self.symbol_dict.items() if len(val) == 1
+        }
+        self.singleton_dict_inv = {
+            val: key for key, val in self.singleton_dict.items()
+        }
+
+    def validate_string(self, dist_str: str) -> str:
+        self.generate_distribution(dist_str)
+        return dist_str
+
+    def generate_distribution(self, dist_str: str) -> Distribution[T]:
+        """
+        Generate a distribution from a string.  The format is:
+            "weight1: [symbol_1, symbol_2,...], weight2: [symbol_1, ...]"
+        """
+
+        dist_str = "".join(dist_str.split())
+        wo_pairs: list[tuple[float, ObjType]] = []
+
+        while dist_str:
+            parts = dist_str.split(":", maxsplit=1)
+            weight = float(parts[0])
+
+            rest = parts[1]
+            if rest[0] == "[":
+                vals_str, rest = rest[1:].split("]", maxsplit=1)
+                if rest and rest[0] == ",":
+                    rest = rest[1:]
+            else:
+                rest_parts = rest.split(",", maxsplit=1)
+                vals_str = rest_parts[0]
+                if len(rest_parts) == 2:
+                    rest = rest_parts[1]
+                else:
+                    rest = ""
+
+            vals = list(itertools.chain.from_iterable(self.symbol_dict[x] for x in vals_str.split(",")))
+            wo_pairs.append((weight, vals))
+
+            dist_str = rest
+
+        return Distribution[T](*wo_pairs)
+
+
+def main():
+    """Sample for using Distribution and DistributionGenerator"""
+    from ctrando.common.ctenums import ItemID
+
+    symbol_dict = {
+        "wood_sword": [ItemID.WOOD_SWORD],
+        "iron_sword": [ItemID.IRON_SWORD],
+        "bad_wpns": [ItemID.MOP, ItemID.GRAEDUS],
+        "good_wpns": [ItemID.RAINBOW, ItemID.CRISIS_ARM],
+    }
+
+    dist_str = "10: wood_sword, 20: [wood_sword, bad_wpns], 5: good_wpns"
+    gen = DistributionGenerator(symbol_dict)
+    dist = gen.generate_distribution(dist_str)
+
+    rng = random.Random("asdf")
+
+    for _ in range(10):
+        print(dist.get_random_item(rng))
+
+
+if __name__ == "__main__":
+    main()
