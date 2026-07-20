a06c6dc785d9cf1ca0081138e814988a2530f542
Anguirel <anguirel86@gmail.com>
2026-07-20 07:58:51 -0400

    Fix region connector issue
    Added back the porre -> choras connector, commented out the duplicate porre -> porre

--- src/ctrando/entranceshuffler/regionmap.py
+++ src/ctrando/entranceshuffler/regionmap.py
@@ -990,8 +990,15 @@ def get_default_region_connectors(
                                        logictypes.ScriptReward.FLIGHT]),
             reversible=False,
         ),
+        #RegionConnector(
+        #    "porre_1000_overworld", "porre_600_overworld",
+        #    "flight_600",
+        #    rule=logictypes.LogicRule([logictypes.ScriptReward.EPOCH,
+        #                               logictypes.ScriptReward.FLIGHT]),
+        #    reversible=False,
+        #),
         RegionConnector(
-            "porre_1000_overworld", "porre_600_overworld",
+            "porre_1000_overworld", "choras_600_overworld",
             "flight_600",
             rule=logictypes.LogicRule([logictypes.ScriptReward.EPOCH,
                                        logictypes.ScriptReward.FLIGHT]),
