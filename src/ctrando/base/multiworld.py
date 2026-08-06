1b69290e16a8c3342ae40bd43528d7d1a2430485
Anguirel <anguirel86@gmail.com>
2026-08-06 17:51:35 -0400

    Multiworld updates

--- src/ctrando/base/multiworld.py
+++ src/ctrando/base/multiworld.py
@@ -1,9 +1,21 @@
 """Allow items to be gained by setting certain memory."""
 
+from typing import Optional
+
 from ctrando.asm import assemble
 from ctrando.asm import instructions as inst
 from ctrando.asm.instructions import AddressingMode as AM
-from ctrando.common import asmpatcher, byteops, ctrom, freespace
+from ctrando.base.openworldutils import CommandNotFoundException
+from ctrando.common import (
+    asmpatcher,
+    byteops,
+    ctenums,
+    ctrom,
+    freespace,
+    memory,
+    randostate,
+)
+from ctrando.locations.eventcommand import EventCommand
 
 ROM_VALIDATION_ADDR = 0x3F8C03
 
@@ -48,19 +60,42 @@ def _patch_remote_items(
     ct_rom.seek(0x0000AD)
     ct_rom.write(inst.JSL(rom_addr, AM.LNG).to_bytearray())
 
+def _add_victory_flag(ct_rom: ctrom.CTRom, script_manager: randostate.ScriptManager):
+    """
+    Add a victory flag to the ending selector scene so that the AP client
+    can detect when the player finishes the game.
+    """
+    script = script_manager[ctenums.LocID.ENDING_SELECTOR_052]
+
+    # Add our flag to the beginning of the ending selector startup
+    cmd = EventCommand.set_explore_mode(False)
+    pos = script.find_exact_command(cmd)
+
+    if pos is None:
+        raise CommandNotFoundException
+
+    pos = pos + len(cmd)
+    victory_flag = memory.Flags.VICTORY_FLAG
+    victory_flag_cmd = EventCommand.set_bit(victory_flag.value.address, victory_flag.value.bit)
+    script.insert_commands(victory_flag_cmd.to_bytearray(), pos)
+
+
 def write_player_validation_data(ct_rom: ctrom.CTRom, encoded_name: bytes):
     """Write the player validation data to the ROM"""
     ct_rom.seek(ROM_VALIDATION_ADDR)
     ct_rom.write(b"APRDI" + encoded_name)
 
-def apply_multiworld_patches(ct_rom: ctrom.CTRom):
+def apply_multiworld_patches(ct_rom: ctrom.CTRom, script_manager: randostate.ScriptManager):
     """Apply the  multiworld related patches and changes"""
 
     # Reserve memory for player validation
-    block = (ROM_VALIDATION_ADDR, 0x20)
-    if not ct_rom.space_manager.is_block_free(block):
-        raise freespace.FreeSpaceError(f"Multiworld validation block already in use: {block}")
+    block = (ROM_VALIDATION_ADDR, ROM_VALIDATION_ADDR + 0x20)
+    MARK_FREE = ctrom.freespace.FSWriteType.MARK_FREE
+    ct_rom.space_manager.mark_block(block, MARK_FREE)
+    #if not ct_rom.space_manager.is_block_free(block):
+    #    raise freespace.FreeSpaceError(f"Multiworld validation block already in use: {block}")
 
     ct_rom.space_manager.mark_block(block, freespace.FSWriteType.MARK_USED)
 
     _patch_remote_items(ct_rom, 0x7F003B, 0x7F0039)
+    _add_victory_flag(ct_rom, script_manager)
